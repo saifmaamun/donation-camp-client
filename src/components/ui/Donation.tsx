59e8e188c4fd59c3c7aeb31e77ccc2c19a63e27f
Saif Maamun <sadifmamun@gmail.com>
2024-02-11 01:14:19 +0600

    polished all the pages

--- src/components/ui/Donation.tsx
+++ src/components/ui/Donation.tsx
@@ -10,8 +10,18 @@ const Donation = ({ donations }: { donations: TDonation[] }) => {
           key={donation._id}
           className="rounded-2xl flex flex-col items-start justify-between p-5 overflow-hidden shadow-md border border-gray-100 hover:shadow-2xl hover:scale-[102%] transition-all"
         >
-          <h1 className="text-2xl font-semibold">{donation?.title}</h1>
-          <p className="my-3">{donation?.details}</p>
+          <h2 className="font-semibold">
+            {donation?.title.length > 30
+              ? donation?.title.slice(0, 30)
+              : donation?.title}
+            ...
+          </h2>
+          <p className="my-3">
+            {donation?.details.length > 150
+              ? donation?.details.slice(0, 150)
+              : donation?.details}
+            ...
+          </p>
           <p>
             <span className="font-semibold">Price:</span>{" "}
             {donation?.donation_amount}
